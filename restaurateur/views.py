65744c42480e5e951404a84a4fb478fbb907c531
KamenevArtem <kamenevart94@gmail.com>
2023-05-21 17:38:56 +0300

    divide show_order function to subfunctions

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -1,3 +1,5 @@
+import requests
+
 from django import forms
 from django.shortcuts import redirect, render
 from django.views import View
@@ -5,8 +7,6 @@ from django.urls import reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
-
-from pprint import pprint
 
 from foodcartapp.models import Product, Restaurant, Order, RestaurantMenuItem
 
@@ -63,6 +63,53 @@ def is_manager(user):
     return user.is_staff  # FIXME replace with specific permission
 
 
+def fetch_restaurant_menu():
+    available_menu = RestaurantMenuItem.objects \
+        .prefetch_related('restaurant', 'product') \
+        .filter(availability=True) \
+        .order_by('product') \
+        .values_list('product', 'restaurant')
+    restaurant_products = {
+            product: set() for product, restaurant in available_menu
+        }
+    for product, restaurant in available_menu:
+        restaurant_products[product].add(restaurant)
+    return restaurant_products
+
+
+def create_order_description(order, restaurant_products, restaurants):
+    orders_to_show_descriptions = {
+        'id': order.id,
+        'status': order.get_status_display(),
+        'payment': order.get_payment_display(),
+        'client': f'{order.firstname} {order.lastname}',
+        'phone': f'+{order.phonenumber.country_code}'
+                 f'{order.phonenumber.national_number}',
+        'address': order.address,
+        'order_price': f'{order.order_price} рублей',
+        'comment': order.comment,
+        'restaurant': order.restaurant,
+    }
+    order_elements = order.elements.select_related('product')
+    available_restaurants = []
+    if order_elements != 0:
+        suitable_restaurants_ids = set.intersection(
+                *[restaurant_products[order_element.product.id] for order_element in order_elements]
+            )
+        if order.restaurant:
+            available_restaurants = order.restaurant.name
+            orders_to_show_descriptions['restaurants'] = available_restaurants
+            order.status = 'P'
+            orders_to_show_descriptions['status'] = order.get_status_display()
+            order.save()
+        else:
+            for restaurant in restaurants:
+                if restaurant.id in suitable_restaurants_ids:
+                    available_restaurants.append(restaurant.name)
+            orders_to_show_descriptions['restaurants'] = available_restaurants
+    return orders_to_show_descriptions
+
+
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_products(request):
     restaurants = list(Restaurant.objects.order_by('name'))
@@ -93,50 +140,18 @@ def view_restaurants(request):
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
     restaurants = Restaurant.objects.all()
+    restaurant_products = fetch_restaurant_menu()
     orders = Order.objects \
         .count_order_price() \
         .exclude(status='DN') \
         .order_by('status')
-    available_menu = RestaurantMenuItem.objects \
-        .prefetch_related('restaurant', 'product') \
-        .filter(availability=True) \
-        .order_by('product') \
-        .values_list('product', 'restaurant')
-    restaurant_products = {
-            product: set() for product, restaurant in available_menu
-        }
-    for product, restaurant in available_menu:
-        restaurant_products[product].add(restaurant)
     orders_to_show = []
     for order in orders:
-        orders_to_show_descriptions = {
-            'id': order.id,
-            'status': order.get_status_display(),
-            'payment': order.get_payment_display(),
-            'client': f'{order.firstname} {order.lastname}',
-            'phone': f'+{order.phonenumber.country_code}{order.phonenumber.national_number}',
-            'address': order.address,
-            'order_price': f'{order.order_price} рублей',
-            'comment': order.comment,
-            'restaurant': order.restaurant,
-        }
-        order_elements = order.elements.select_related('product')
-        available_restaurants = []
-        if order_elements != 0:
-            suitable_restaurants_ids = set.intersection(
-                    *[restaurant_products[order_element.product.id] for order_element in order_elements]
-                )
-            if order.restaurant:
-                available_restaurants = order.restaurant.name
-                orders_to_show_descriptions['restaurants'] = available_restaurants
-                order.status = 'P'
-                orders_to_show_descriptions['status'] = order.get_status_display()
-                order.save()
-            else:
-                for restaurant in restaurants:
-                    if restaurant.id in suitable_restaurants_ids:
-                        available_restaurants.append(restaurant.name)
-                orders_to_show_descriptions['restaurants'] = available_restaurants
+        orders_to_show_descriptions = create_order_description(
+            order,
+            restaurant_products,
+            restaurants
+        )
         orders_to_show.append(orders_to_show_descriptions)
     return render(
         request,
