1abf6f8ce7f6b955b310f6dd2bc1a2b633893b90
KamenevArtem <kamenevart94@gmail.com>
2023-09-09 18:52:21 +0300

    Update deploy_star_burger.sh

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,4 +1,13 @@
-set -e
 #!/bin/bash
+set -e
+cd /opt/star-burger/
 git pull
-echo "Deploy comleted"
+source ./venv/bin/activate
+pip3 install -r requirements.txt
+npm ci --dev
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+systemctl restart starburger
+systemctl reload nginx
+-echo "Deploy comleted"
