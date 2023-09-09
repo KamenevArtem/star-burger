8e98bbedc6fd098d094176677ebfba34ca8891be
KamenevArtem <kamenevart94@gmail.com>
2023-09-09 19:02:24 +0300

    Update deploy_star_burger.sh

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
 set -e
 cd /opt/star-burger/
 git pull
-source ../venv/bin/activate
-pip3 install -r requirements_prod.txt
+source ./venv/bin/activate
+pip3 install -r requirements.txt
 npm ci --dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 python3 manage.py collectstatic --noinput
