b5c537a30e43921e3d0398963fe37d6e1feb08ec
KamenevArtem <kamenevart94@gmail.com>
2023-09-09 19:05:31 +0300

    Update deploy_star_burger.sh

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -7,5 +7,6 @@ npm ci --dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
+systemctl daemon-reload
 systemctl restart starburger
 systemctl reload nginx
